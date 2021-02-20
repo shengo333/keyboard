0bad09f576105afe4525db36c5c32fd530edd3f1
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-02-20 11:38:05 +0400

    finished

---
+++ calculator.js
@@ -0,0 +1,37 @@
+
+function sum(){
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+    var output = document.getElementById("output");
+    output.innerHTML = parseInt(input1) + parseInt(input2);
+
+    console.log(input1)
+    console.log(input2)
+    console.log(output)
+
+}
+function substract(){
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+    var output = document.getElementById("output");
+    output.innerHTML = parseInt(input1) - parseInt(input2);
+
+    console.log(input1)
+    console.log(input2)
+    console.log(output)
+
+}
+function devide(){
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+    var output = document.getElementById("output");
+    output.innerHTML = parseInt(input1) / parseInt(input2);
+
+    console.log(input1)
+    console.log(input2)
+    console.log(output)
+
+}
+
+
+
