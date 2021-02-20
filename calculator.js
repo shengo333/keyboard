05097ed4c9b67800b9a26c2fe8f2245cad322018
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-02-20 15:13:29 +0400

    calculator

--- calculator.js
+++ calculator.js
@@ -32,6 +32,15 @@ function devide(){
     console.log(output)
 
 }
+function multiply(){
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+    var output = document.getElementById("output");
+    output.innerHTML = parseInt(input1) * parseInt(input2);
 
+    console.log(input1)
+    console.log(input2)
+    console.log(output)
+}
 
 
